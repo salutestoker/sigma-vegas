911797d29f0220d1ce845ec8765aa9f1806e7055
salutestoker <salutestoker@gmail.com>
2026-04-01 13:35:15 -0400

    site title meta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,15 +47,10 @@ export default function Home() {
                             <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
-                                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-                            />
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
                             />
                         </svg>
-                        Hard Rock Cafe
+                        7pm
                     </div>
                     <div className="flex items-center gap-1 bg-[#e935cb95] px-6 py-2">
                         <svg
@@ -69,10 +64,15 @@ export default function Home() {
                             <path
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
-                                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
                             />
                         </svg>
-                        7pm
+                        Hard Rock Cafe
                     </div>
                 </div>
             </div>
