6b5f1882679343df0497a77dbc75ae3da01919d3
salutestoker <salutestoker@gmail.com>
2026-04-01 13:22:09 -0400

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,92 @@
+import Image from 'next/image';
+
+export default function Home() {
+    return (
+        <div className="relative h-dvh w-dvw overflow-hidden font-sans dark:bg-black">
+            <div className="absolute top-1/2 left-1/2 z-20 flex -translate-1/2 flex-col gap-3 text-black">
+                <div className="flex items-center justify-center gap-1 bg-[#e935cb95] px-6 py-1">
+                    LAS VEGAS AFTER PARTY
+                </div>
+                <div className="bg-[#e935cb95] py-6">
+                    <Image
+                        className="mx-auto invert"
+                        src="/images/main_sigma_logo_white.png"
+                        alt="Sigma logo"
+                        width={400}
+                        height={212}
+                        priority
+                    />
+                </div>
+                <div className="flex justify-stretch gap-2">
+                    <div className="flex items-center gap-1 bg-[#e935cb95] px-6 py-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z"
+                            />
+                        </svg>
+                        Thursday - April 30th, 2026
+                    </div>
+                    <div className="flex items-center gap-1 bg-[#e935cb95] px-6 py-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                            />
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                            />
+                        </svg>
+                        Hard Rock Cafe
+                    </div>
+                    <div className="flex items-center gap-1 bg-[#e935cb95] px-6 py-2">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="size-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                            />
+                        </svg>
+                        7pm
+                    </div>
+                </div>
+            </div>
+            <div className="absolute top-0 left-0 z-10 h-full w-full bg-black/75"></div>
+            <div>
+                <video
+                    className="h-full w-full object-cover"
+                    src="/videos/sigma-vegas-intro.mp4"
+                    autoPlay
+                    loop
+                    muted
+                    playsInline
+                />
+            </div>
+        </div>
+    );
+}
